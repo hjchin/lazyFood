da235a8a1d0f0a3a5de2069bf21eabc325b70c36
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-09-12 22:41:04 +0800

    Add the recycler view

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.2.1")
     implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.1")
     implementation("androidx.core:core-ktx:1.3.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
@@ -30,10 +30,11 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
     implementation("com.github.esafirm.android-image-picker:imagepicker:2.3.2")
-    implementation("com.github.bumptech.glide:glide:4.5.0")
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.11.0")
     implementation("com.jakewharton.timber:timber:4.7.1")
 
-
+    implementation("rouchuan.viewpagerlayoutmanager:viewpagerlayoutmanager:2.0.22")
 }
 android {
     compileSdkVersion(29)
--- androidApp/src/main/java/world/trav/lazyfood/androidApp/ui/main/DefaultFragment.kt
+++ androidApp/src/main/java/world/trav/lazyfood/androidApp/ui/main/DefaultFragment.kt
@@ -1,19 +1,43 @@
 package world.trav.lazyfood.androidApp.ui.main
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.esafirm.imagepicker.features.ImagePicker
+import com.leochuan.CenterSnapHelper
+import com.leochuan.ScaleLayoutManager
+import com.leochuan.ScrollHelper
+import com.leochuan.ViewPagerLayoutManager
 import timber.log.Timber
 import world.trav.lazyfood.androidApp.R
 import world.trav.lazyfood.androidApp.databinding.DefaultFragmentBinding
+import world.trav.lazyfood.androidApp.databinding.GalleryItemBinding
+import kotlin.math.roundToInt
 
 
 class DefaultFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var binding: DefaultFragmentBinding
+    private var rotateCarousel: Boolean = false
+    private lateinit var scaleLayoutManager: MyScaleLayoutManager
+    private lateinit var handler: Handler
+    private val timeInterval = 100L
+    private val autoPlayRunnable = object : Runnable {
+        override fun run() {
+            val currentPosition =
+                scaleLayoutManager.getCurrentPositionOffset() * if (scaleLayoutManager.reverseLayout) -1 else 1
+            val delta = scaleLayoutManager.getOffsetToPosition(currentPosition + 1)
+            binding.recyclerView.smoothScrollBy(delta, 0)
+            handler.postDelayed(this, timeInterval)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,9 +61,41 @@ class DefaultFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.fab.setOnClickListener {
+        handler = Handler(Looper.getMainLooper())
+        scaleLayoutManager = MyScaleLayoutManager(requireContext(), dp2px(requireContext(), 1f))
 
+        val resource = arrayListOf(
+            R.drawable.food1,
+            R.drawable.food2,
+            R.drawable.food3,
+            R.drawable.food4,
+            R.drawable.food5,
+        )
+
+        binding.message.text = resource.size.toString() + " foods selection"
+
+        scaleLayoutManager.minAlpha = 0.2f
+        scaleLayoutManager.infinite = true
+        scaleLayoutManager.moveSpeed = 5f
+        binding.recyclerView.layoutManager = scaleLayoutManager
+        binding.recyclerView.adapter = GalleryAdapter(this, resource)
+
+        binding.fab.setOnClickListener {
+            if(!rotateCarousel) {
+                rotateCarousel = true
+            handler.postDelayed(autoPlayRunnable, timeInterval)
+            }else{
+                rotateCarousel = false
+                handler.removeCallbacks(autoPlayRunnable)
+            }
         }
+
+        CenterSnapHelper().attachToRecyclerView(binding.recyclerView)
+    }
+
+    private fun dp2px(context: Context, dp: Float): Int {
+        val scale: Float = context.resources.displayMetrics.density
+        return (dp * scale + 0.5f).toInt()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -57,8 +113,48 @@ class DefaultFragment : Fragment() {
             }
             else -> super.onOptionsItemSelected(item)
         }
-
-
     }
 
+    companion object {
+
+        class MyScaleLayoutManager(context: Context, itemSpace: Int) : ScaleLayoutManager(
+            context,
+            itemSpace
+        ) {
+            fun getCurrentPositionOffset(): Int {
+                if (mInterval.toInt() == 0) return 0;
+                return (mOffset / mInterval).roundToInt();
+            }
+        }
+
+        class GalleryAdapter(private val fragment: Fragment, private val urls: List<Int>) :
+            RecyclerView.Adapter<GalleryAdapter.ViewHolder>() {
+
+            class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+            }
+
+            private lateinit var binding: GalleryItemBinding;
+
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+                binding = GalleryItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return ViewHolder(binding.root)
+            }
+
+            override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+                Timber.d("loading " + urls[position])
+                Glide.with(fragment).load(urls[position]).into(binding.imageView)
+            }
+
+            override fun getItemCount(): Int {
+                return urls.size
+            }
+
+        }
+
+    }
 }
